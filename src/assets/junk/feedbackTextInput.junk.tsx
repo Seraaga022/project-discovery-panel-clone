7e4654aa46648b112ee2826ef2b65697199e1715
mhmd ali <serever100@gmail.com>
2024-12-28 19:55:02 +0330

    refactor(junk): commit junk file to prevent build errors
    feedbackTextInput.junk.tsx component

--- src/assets/junk/feedbackTextInput.junk.tsx
+++ src/assets/junk/feedbackTextInput.junk.tsx
@@ -1,30 +1,30 @@
-import { TextField } from "@mui/material";
+// import { TextField } from "@mui/material";
 
-<TextField
-  placeholder="Add a title"
-  sx={{
-    width: "100%",
-    maxHeight: "40px",
-    display: "flex",
-    justifyContent: "center",
-    borderRadius: "7px",
-    bgcolor: "#0e0e10",
-    border: "1px solid #19191b",
-    p: "1px",
-    "& .MuiInputBase-input": {
-      color: "#b2b6cd",
-    },
-    "& .MuiOutlinedInput-notchedOutline": {
-      border: "none",
-      borderRadius: "7px",
-    },
-    "& .Mui-focused .MuiOutlinedInput-notchedOutline": {
-      mt: "6px",
-      mb: "3px",
-      px: "18px",
-      maxHeight: "40px",
-      transition: "border ease-in-out 300ms",
-      border: "3px solid #141423",
-    },
-  }}
-></TextField>;
+// <TextField
+//   placeholder="Add a title"
+//   sx={{
+//     width: "100%",
+//     maxHeight: "40px",
+//     display: "flex",
+//     justifyContent: "center",
+//     borderRadius: "7px",
+//     bgcolor: "#0e0e10",
+//     border: "1px solid #19191b",
+//     p: "1px",
+//     "& .MuiInputBase-input": {
+//       color: "#b2b6cd",
+//     },
+//     "& .MuiOutlinedInput-notchedOutline": {
+//       border: "none",
+//       borderRadius: "7px",
+//     },
+//     "& .Mui-focused .MuiOutlinedInput-notchedOutline": {
+//       mt: "6px",
+//       mb: "3px",
+//       px: "18px",
+//       maxHeight: "40px",
+//       transition: "border ease-in-out 300ms",
+//       border: "3px solid #141423",
+//     },
+//   }}
+// ></TextField>;
