bd8ac772e04b44da6ce5c5dc0125fc4f614e253a
mhmd ali <serever100@gmail.com>
2024-12-28 20:53:07 +0330

    chore: separate vendor libraries chunks from component's
    add manualChunks prop to build.rollupOptions.output prop at vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor"; // Split vendor libraries
+          }
+          if (id.includes("src/components/")) {
+            return "components"; // Split components into their own chunk
+          }
+        },
+      },
+    },
+  },
+});
