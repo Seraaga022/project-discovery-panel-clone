7dcdb322ed7fcb7ac620e7c707875cd3e64d58f0
mhmd ali <serever100@gmail.com>
2024-12-28 12:00:16 +0330

    refactor(ui): update userApiKey component design based on source website

--- src/components/NavBar/UserApiKeyComponent.tsx
+++ src/components/NavBar/UserApiKeyComponent.tsx
@@ -1,10 +1,12 @@
-import { lazy, Suspense } from "react";
+import { Suspense, useState } from "react";
 import { Box, Typography } from "@mui/material";
 import Circular from "../atoms/Loaders/Circular";
 import { UserApiKeyT } from "../../hooks/useUserApiKey";
 import CustomButton from "../atoms/CustomButton/CustomButton";
 import { Clear } from "@mui/icons-material";
-const UserApiKeyAndCopy = lazy(() => import("./UserApiKeyAndCopy"));
+import CopyToClipboard from "react-copy-to-clipboard";
+import { TiTick } from "react-icons/ti";
+import { IoCopyOutline } from "react-icons/io5";
 
 const UserApiKeyComponent = ({
   userApiKey,
@@ -13,6 +15,8 @@ const UserApiKeyComponent = ({
   userApiKey: UserApiKeyT;
   setUserApiKeyDialogState: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
+  const [isApiKeyCopied, setIsApiKeyCopied] = useState(false);
+
   return (
     <Box
       minHeight="400px"
@@ -66,13 +70,17 @@ const UserApiKeyComponent = ({
       >
         <Box display="flex" flexDirection="column" alignItems="center">
           <Box>
-            <Typography variant="h6" sx={{ color: "#ffffff" }}>
+            <Typography variant="h6" sx={{ color: "#ffffff", fontWeight: 500 }}>
               Your API Key
             </Typography>
           </Box>
           <Box>
             <Typography variant="subtitle2" sx={{ color: "#a1a1aa" }}>
-              Rotate or copy your API key
+              You can find documentation{" "}
+              <Box component="span" color="rgb(165 180 252)">
+                here
+              </Box>
+              .
             </Typography>
           </Box>
         </Box>
@@ -80,21 +88,85 @@ const UserApiKeyComponent = ({
       {/* api key and copy */}
       <Box px="15px" alignItems="center">
         <Suspense fallback={<Circular />}>
-          <UserApiKeyAndCopy apiKey={userApiKey} />
+          <Box
+            py="6px"
+            border="1px solid #27272a"
+            borderRadius="7px"
+            bgcolor="rgba(199,210,254,.05)"
+            color="rgb(129 140 248)"
+            boxSizing="border-box"
+            display="flex"
+            paddingRight="15px"
+          >
+            <Box
+              flex={1}
+              paddingInline="20px"
+              display="flex"
+              alignItems="center"
+            >
+              <Typography
+                variant="default"
+                sx={{
+                  userSelect: "text",
+                  fontSize: ".9rem",
+                  fontFamily: "consolas",
+                }}
+              >
+                {userApiKey}
+              </Typography>
+            </Box>
+            <Box
+              flex={0.4}
+              display="flex"
+              justifyContent="end"
+              alignItems="center"
+            >
+              <CopyToClipboard text={userApiKey}>
+                <CustomButton
+                  sx={{
+                    "&:hover": { bgcolor: "#252529" },
+                    "&:hover .copy-tick-icon": { color: "#d9d9dd" },
+                    borderRadius: "5px",
+                    py: "10px",
+                    px: "8px",
+                  }}
+                  onClick={() => {
+                    setIsApiKeyCopied(true);
+                    setTimeout(() => {
+                      setIsApiKeyCopied(false);
+                    }, 2000);
+                  }}
+                >
+                  {isApiKeyCopied ? (
+                    <TiTick
+                      className="copy-tick-icon"
+                      size="14px"
+                      color="#71717a"
+                    />
+                  ) : (
+                    <IoCopyOutline
+                      className="copy-tick-icon"
+                      size="14px"
+                      color="#71717a"
+                    />
+                  )}
+                </CustomButton>
+              </CopyToClipboard>
+            </Box>
+          </Box>
         </Suspense>
       </Box>
       {/* rotate api key button */}
-      <Box height="15%" boxSizing="border-box" padding="15px">
+      <Box height="15%" boxSizing="border-box" px="15px" pt="10px" pb="15px">
         <CustomButton
           fullWidth
           sx={{
-            bgcolor: "#eeeeee",
-            "&:hover": { bgcolor: "#d7d7d7" },
-            color: "#18181b",
-            py: "10px",
+            border: "1px solid #27272a",
+            "&:hover": { bgcolor: "#27272a" },
+            py: "8px",
           }}
         >
-          <Typography fontSize="14px" fontWeight={500}>
+          <Typography fontSize="14px" color="#fff" fontWeight={600}>
             Rotate API Key
           </Typography>
         </CustomButton>
