15a172ac19c96ef12881d31bfbc1bb942c04fa52
mhmd ali <serever100@gmail.com>
2024-12-31 23:42:19 +0330

    chore: install sooner for toasts and place its provider
    src/Pages/Layout.tsx file

--- src/components/pages/Layout.tsx
+++ src/components/pages/Layout.tsx
@@ -3,6 +3,7 @@ import Navbar from "../NavBar/Navbar";
 import { Outlet } from "react-router-dom";
 import { AppTheme } from "../../theme/AppTheme";
 import type { UserT } from "@appTypes/types/user";
+import { Toaster } from "sonner";
 
 const Layout = () => {
   const user: UserT = {
@@ -16,6 +17,7 @@ const Layout = () => {
 
   return (
     <ThemeProvider theme={AppTheme}>
+      <Toaster />
       <Box minWidth="1300px">
         <Navbar user={user} />
         <Box id="rest">
